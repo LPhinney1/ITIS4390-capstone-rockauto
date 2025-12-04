ff0d34cc8c80da27a68b542b6e9e0550a5b4d676
LPhinney1 <lphinne1@uncc.edu>
2025-12-04 16:30:49 -0500

    Squash merge dev-logan into staging

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,5 +3,8 @@ module.exports = {
     singleQuote: true,
     trailingComma: 'all',
     tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    arrowParens: 'always',
     plugins: ['prettier-plugin-tailwindcss'],
 };
