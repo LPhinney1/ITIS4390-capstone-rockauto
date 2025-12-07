7269779d6571ed2e19681e5041744e8e30827ec9
LPhinney1 <lphinne1@uncc.edu>
2025-12-07 16:51:48 -0500

    minor css fixes
    add some top-margin to footer
    recolor advanced-search button
    make continue shopping not waste so much space

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="mx-48 my-20">
-            <div className="-mx-10">
+        <div className="mx-48">
+            <div className="-mx-10 mt-6 mb-3">
                 <Link
                     href="/"
                     className="flex items-center gap-2 text-gray-600 transition-colors hover:text-gray-900"
                 >
-                    ← Continue Shopping
+                    <b>← Continue Shopping</b>
                 </Link>
             </div>
             {children}
